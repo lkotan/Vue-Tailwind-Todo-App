0aafebc8b1bef73f41573cf239734539fa284a99
lKotan <lutfikotann@gmail.com>
2021-09-26 15:35:34 +0300

    Search component

---
+++ src/components/Search.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" class="search" placeholder="Search..." @keyup="$emit('searchEvent',$event)" />
+</template>
+
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<style>
+.search {
+  @apply w-full
+  mt-6
+  border-none
+  outline-none
+  p-2
+  rounded-sm
+  font-bold
+  text-white
+  bg-gray-700;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,11 +2,12 @@
   <div class="todos">
     <header class="header">
       <h5>Todo App Vue-Tailwind</h5>
-      <Button class="add" name="Add" @eventClick="$router.push('/todo')"/>
+      <Button class="add" name="Add" @eventClick="$router.push('/todo')" />
     </header>
-    <div v-if="todos.length != 0">
+    <Search @searchEvent="searchEvent" v-if="filteredData.length != 0 || search!=''"/>
+    <div v-if="filteredData.length != 0">
       <Todo
-        v-for="todo in todos"
+        v-for="todo in filteredData"
         :key="todo.id"
         :todo="todo"
         @getData="getData"
@@ -19,23 +20,34 @@
 <script>
 import Todo from "@/components/Todo";
 import Button from "@/components/Button";
+import Search from "@/components/Search";
 
 export default {
   name: "Todos",
   components: {
     Todo,
     Button,
+    Search,
   },
   data() {
     return {
       todos: [],
+      search:""
     };
+  },
+  computed:{
+    filteredData(){
+      return this.todos.filter(x=>x.name.toLowerCase().includes(this.search.toLowerCase()))
+    }
   },
   methods: {
     async getData() {
       const res = await fetch(`${this.$baseURL}/todos`);
       const data = await res.json();
       this.todos = data;
+    },
+    searchEvent(e) {
+      this.search=e.target.value;
     },
   },
   async created() {
